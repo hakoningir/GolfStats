7036f7414821e871ad2ffc504315d7cd9b4f62e4
hakoningir <hir12@hi.is>
2023-04-16 13:52:46 +0000

    login bras

--- components/loginForm.js
+++ components/loginForm.js
@@ -9,7 +9,7 @@ const LoginForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("/api/login", {
+    const response = await fetch("/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ username, password }),
